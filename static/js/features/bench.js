79c2fa7716c16826b6295621ca998e70ada53b37
Joseph Kato <joseph@jdkato.io>
2020-01-17 16:34:03 -0800

    Make charts mobile-friendly

--- static/js/features/bench.js
+++ static/js/features/bench.js
@@ -1,92 +1,18 @@
-if ($("#benchmark-formats").length) {
-    Highcharts.chart('benchmark-formats', {
-        exporting: {
-            buttons: null,
-        },
-        credits: false,
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Build Time by Format (3-run average)'
-        },
-        subtitle: {
-            text: versionInfo
-        },
-        xAxis: {
-            categories: [
-                '10 files',
-                '100 files',
-                '1,000 files'
-            ],
-            crosshair: true
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Build Time (seconds)'
-            }
-        },
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b> {point.y:.1f} s</b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: seriesData1
-    });
-}
-
-var formatOptions = [];
-if (hasMd) {
-    formatOptions.push({
-        text: 'Markdown',
-        onclick: function () {
-            rebuildChart('Markdown')
-        }
-    });
-}
-if (hasAdoc) {
-    formatOptions.push({
-        text: 'AsciiDoc',
-        onclick: function () {
-            rebuildChart('AsciiDoc')
-        }
-    });
-}
-if (hasRst) {
-    formatOptions.push({
-        text: 'reStructuredText',
-        onclick: function () {
-            rebuildChart('reStructuredText')
-        }
-    });
-}
-
-var options = {
+var ssgOptions = {
     credits: false,
-    title: {
-        text: 'Build Time by Generator'
-    },
-
-    subtitle: {
-        text: 'Markdown'
-    },
-
-    yAxis: {
-        title: {
-            text: 'Build Time (seconds)'
+    chart: {
+        type: 'column',
+        scrollablePlotArea: {
+            minWidth: 576,
+            scrollPositionX: 1
         }
     },
-
+    title: {
+        text: 'Build Time by Format (3-run average)'
+    },
+    subtitle: {
+        text: versionInfo
+    },
     xAxis: {
         categories: [
             '10 files',
@@ -95,41 +21,97 @@ var options = {
         ],
         crosshair: true
     },
-
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Build Time (seconds)'
+        }
     },
-
-     exporting: {
-         buttons: {
-             contextButton: {
-                 enabled: false
-             },
-             toggle: {
-                 text: 'Format',
-                 menuItems: formatOptions
-             }
-         }
-     },
-
-    series: md
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b> {point.y:.1f} s</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: seriesData1
 };
 
-function rebuildChart(format) {
-    console.log(format);
+function getOptions(format) {
+    return {
+        credits: false,
+        title: {
+            text: 'Build Time by Generator'
+        },
 
-    formatsChart.destroy();
+        subtitle: {
+            text: format
+        },
 
-    options.subtitle.text = format;
-    options.series = formatData[format];
+        yAxis: {
+            title: {
+                text: 'Build Time (seconds)'
+            }
+        },
 
-    formatsChart = new Highcharts.chart('benchmark-ssgs', options);
+        xAxis: {
+            categories: [
+                '10 files',
+                '100 files',
+                '1,000 files'
+            ],
+            crosshair: true
+        },
+
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 576
+                },
+                chartOptions: {
+                    legend: {
+                        align: 'center',
+                        verticalAlign: 'bottom',
+                        layout: 'horizontal'
+                    }
+                }
+            }]
+        },
+
+        chart: {
+            scrollablePlotArea: {
+                minWidth: 576,
+                scrollPositionX: 1
+            }
+        },
+
+        series: formatData[format]
+    };
 }
 
-var formatsChart = Highcharts.chart('benchmark-ssgs', options);
+var ssgChart = Highcharts.chart('benchmark-formats', ssgOptions);
+var formatChart = Highcharts.chart('Markdown', getOptions('Markdown'));
+
 $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-  formatsChart.destroy();
-  formatsChart = new Highcharts.chart('benchmark-ssgs', options);
+    var selText = $(this).text();
+    if (selText !== 'By Format') {
+        formatChart.destroy();
+        formatChart = Highcharts.chart(selText, getOptions(selText));
+    } else {
+        ssgChart.destroy();
+        ssgChart = Highcharts.chart('benchmark-formats', ssgOptions);
+    }
 });
